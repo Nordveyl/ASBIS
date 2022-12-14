8431e9e0fa5a2da5881dd2ec97bd1c52711b8db9
Nordveyl <kkrutoychel.wf@gmail.com>
2022-12-15 02:00:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import socket
+from ins_and_sns import INS, SNS
+from ctypes import *
+
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 12346
@@ -8,8 +11,25 @@ server = socket.socket(socket.AF_INET, # Internet
                        socket.SOCK_DGRAM) # UDP
 server.bind((addr)) # binds the address and port to the socket
 
+def decode(value, n_bit, senior_bit):
+    return senior_bit * value / 2 ** (n_bit - 1)
+
+def unpacking(type, buffer):
+    buff = create_string_buffer(buffer)
+    buff_real = cast(pointer(buff), POINTER(type)).contents
+    return buff_real
+
 while True:
-    data = float(server.recv(1024).decode('utf-8')) # buffer size is 1024 bytes and decode from utf-8
-    print(data)
+    data = server.recv(1024)
+
+    if len(data) == 52:
+        data_length = unpacking(INS, data)
+        print(len(data))
+        print(decode(data_length.latitude.value, 20, 90))
+
+    elif len(data) == 56:
+        data_length = unpacking(SNS, data)
+        print(len(data))
+        print(decode(data_length.L.value, 20, 90))
 
 server.close()
