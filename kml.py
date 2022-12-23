4396ba8956a55d50b1ae35cd751a0c26e22e568a
Nordveyl <kkrutoychel.wf@gmail.com>
2022-12-23 17:29:44 +0300

    Update kml.py

--- kml.py
+++ kml.py
@@ -14,7 +14,27 @@ class KML:
         f.close()
         return coordinates
 
-    def write_coordinates_to_kml_file(massiv_s_coord, file):
+    def write_coordinates_to_kml_file(crds_1, crds_2, file, color1, color2):
         f = simplekml.Kml()
-        f.newlinestring(tessellate="uiyf", coords=[x for x in massiv_s_coord])
+        line1 = f.newlinestring(tessellate="1", coords=[x for x in crds_1])
+        line2 = f.newlinestring(tessellate="2", coords=[x for x in crds_2])
+        line1.style.linestyle.color = color1
+        line2.style.linestyle.color = color2
         f.save(f'{file}')
+
+    def choose_color(object):
+        print(f"choose the color of the {object} trajectory")
+        flag = input("1 Blue \n2 White \n3 Black \n4 Red \n5 Yellow \n ")
+        match flag:
+            case "1":
+                color = simplekml.Color.blue
+            case "2":
+                color = simplekml.Color.white
+            case "3":
+                color = simplekml.Color.black
+            case "4":
+                color = simplekml.Color.red
+            case "5":
+                color = simplekml.Color.yellow
+
+        return color
